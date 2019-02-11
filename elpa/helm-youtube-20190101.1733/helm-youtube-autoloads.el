dea1299f7c49efb58e3e45f97979bdbb973014cb
Lukas Jenks <lukasjenks@gmail.com>
2019-02-11 11:00:19 -0700

    Initial commit

---
+++ elpa/helm-youtube-20190101.1733/helm-youtube-autoloads.el
@@ -0,0 +1,27 @@
+;;; helm-youtube-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-youtube" "helm-youtube.el" (0 0 0 0))
+;;; Generated autoloads from helm-youtube.el
+
+(autoload 'helm-youtube "helm-youtube" "\
+
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-youtube" '("helm-youtube-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-youtube-autoloads.el ends here
---
+++ elpa/org-journal-20190121.909/org-journal-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-journal" "20190121.909" "a simple org-mode based journaling mode" '((emacs "25.1")) :commit "20631b02e892e08e72ba7660ff4d5107c8fb5e61" :authors '(("Bastian Bechtold")) :maintainer '("Bastian Bechtold") :url "http://github.com/bastibe/org-journal")
