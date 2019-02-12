25b997b27f58f23b5436a0df3a8ed2f1e08de4c6
Lukas Jenks <lukasjenks@gmail.com>
2019-02-11 19:59:28 -0700

    Added go autocomplete functionality

--- elpa/go-errcheck-20160723.43/go-errcheck-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-errcheck" "20160723.43" "errcheck integration for go-mode" 'nil :commit "9db21eccecedc2490793f176246094167164af31" :authors '(("Dominik Honnef" . "dominikh@fork-bomb.org")) :maintainer '("Dominik Honnef" . "dominikh@fork-bomb.org"))
