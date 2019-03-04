7516623b6c75a3cc39b4c40e585288c6b7143c9a
Lukas Jenks <lukasjenks@gmail.com>
2019-03-03 23:40:37 -0700

    Disable theme if running in terminal

--- init.el
+++ init.el
@@ -61,7 +61,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (doom-dracula)))
+ (when (display-graphic-p)
+ '(custom-enabled-themes (quote (doom-dracula))))
  '(custom-safe-themes
    (quote
     ("f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" default)))
