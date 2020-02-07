b9716f9201402f889ec34d9a78c6ffc93d84115f
Lukas Jenks <lukasjenks@gmail.com>
2020-02-06 17:46:42 -0700

    Append entries to tabbar ignore list and install ejc-sql

---
+++ elpa/ejc-sql-20200204.1415/ejc-sql-pkg.el
@@ -0,0 +1,17 @@
+(define-package "ejc-sql" "20200204.1415" "Emacs SQL client uses Clojure JDBC."
+  '((emacs "26.3")
+    (clomacs "0.0.4")
+    (dash "2.16.0")
+    (auto-complete "1.5.1")
+    (spinner "1.7.3")
+    (direx "1.0.0"))
+  :keywords
+  '("sql" "jdbc")
+  :authors
+  '(("Kostafey" . "kostafey@gmail.com"))
+  :maintainer
+  '("Kostafey" . "kostafey@gmail.com")
+  :url "https://github.com/kostafey/ejc-sql")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
